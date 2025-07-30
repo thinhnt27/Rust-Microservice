0e73281ae4d3c8795431b6c1dfacae6ef6ee1135
Trường Thịnh <nguyentruongthinh17022003@gmail.com>
2025-07-30 08:26:47 +0700

    config and add error response

---
+++ src/errors.rs
@@ -0,0 +1,28 @@
+use std::env;
+
+use axum::{http::StatusCode, response::IntoResponse, Json};
+use serde_json::json;
+use thiserror::Error;
+
+
+#[derive(Debug, Error)]
+pub enum AppError {
+    #[error("Configuration error")]
+    Config(#[from] config::ConfigError),
+
+    #[error("Not found...")]
+    NotFound,
+
+    #[error("Env Error")]
+    EnvError(#[from] env::VarError),
+}
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> axum::response::Response {
+        match self {
+            AppError::Config(e) => (StatusCode::BAD_REQUEST, Json(json!({"error" : e.to_string()}))).into_response(),
+            AppError::NotFound => (StatusCode::NOT_FOUND, Json(json!({"error": "Not Found..."}))).into_response(),
+            AppError::EnvError(e) => (StatusCode::FORBIDDEN, Json(json!({"error": e.to_string()}))).into_response(),
+        }
+    }
+}
