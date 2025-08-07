a568668c0386ac167e35c74e3d981849cbaaf1bf
Trường Thịnh <nguyentruongthinh17022003@gmail.com>
2025-08-07 09:25:08 +0700

    add middleware

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::sync::Arc;
 
-use axum::{extract::Path, http::StatusCode, response::{IntoResponse, Response}, routing::get, Extension, Json, Router};
+use axum::{extract::{Path, Request}, http::{Method, StatusCode, Uri}, middleware::{self, Next}, response::{IntoResponse, Response}, routing::get, Extension, Json, Router};
 use dotenv::dotenv;
 use rust_microservice::{configs::ProdConfig, dbs::initialed_db, errors::AppError, AppResult};
 use serde_json::json;
 use tracing::info;
+use tracing_subscriber::layer;
+use uuid::Uuid;
 
 #[tokio::main]
 async fn main() {
@@ -14,7 +16,10 @@ async fn main() {
   let cfg = ProdConfig::from_env().expect("Cann't get env");
   let pool = initialed_db(&cfg.postgres.dsn, cfg.postgres.max_conns).await;
 
-  let app = Router::new().route("/:msg", get(say_hello)).layer(Extension(Arc::new(pool)));
+  let app = Router::new().route("/:msg", get(say_hello))
+  .layer(middleware::map_response(mw_map_response))
+  .layer(middleware::from_fn_with_state(pool.clone(), mw_auth))
+  .with_state(Arc::new(pool));
   info!("Connect Database successfully");
 
   info!("Server is running on port: {}", cfg.web.addr);
@@ -23,9 +28,24 @@ async fn main() {
 }
 
 pub async fn say_hello(Path(msg): Path<String>) -> AppResult<Json<serde_json::Value>> {
+  info!("Say helllo!!!");
   if msg.is_empty() {
     Err(AppError::NotFound)
   } else {
     Ok(Json(json!({"message": format!("Hello, {}!", msg)})))
   }
+}
+
+pub async fn mw_map_response(uri: Uri, req_method: Method,res: Response) -> Response {
+    let uuid = Uuid::new_v4();
+    info!("Request ID: {}", uuid);
+    info!("Mapping response");
+    info!("Request URI: {}", uri);
+    info!("Request Method: {}", req_method);
+    (StatusCode::ACCEPTED, res).into_response()
+}
+
+pub async fn mw_auth(req: Request, next: Next) -> AppResult<Response> {
+    info!("Auth Middleware");
+    Ok(next.run(req).await)
 }
